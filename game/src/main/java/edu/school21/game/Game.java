3888e1f75b575998d2f6f5ea6a04224be94e13a0
Daniil <daniil.dementiev007@yandex.ru>
2021-07-04 13:35:15 +0300

    Add argument reading

--- game/src/main/java/edu/school21/game/Game.java
+++ game/src/main/java/edu/school21/game/Game.java
@@ -1,7 +1,51 @@
 package edu.school21.game;
 
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
+@Parameters(separators = "=")
 public class Game {
+
+	@Parameter(
+			names = "--enemiesCount",
+			description = "Amount of enemies",
+			required = true
+	)
+	int enemiesCount;
+	@Parameter(
+			names = "--wallsCount",
+			description = "Amount of walls",
+			required = true
+	)
+	int wallsCount;
+
+	@Parameter(
+			names = "--size",
+			description = "Game map size",
+			required = true
+	)
+	int gameMapSize;
+
+	@Parameter(
+			names = "--profile",
+			description = "Profile name",
+			required = true
+	)
+	String profileName;
+
 	public static void main(String[] args) {
-		System.out.println("I am Game!");
+
+		Game game = new Game();
+		JCommander jCommander = JCommander.newBuilder()
+				.addObject(game)
+				.build();
+		jCommander.parse(args);
+
+		System.out.println(game.enemiesCount);
+		System.out.println(game.wallsCount);
+		System.out.println(game.gameMapSize);
+		System.out.println(game.profileName);
 	}
 }
---
+++ game/src/main/java/edu/school21/game/GameMap.java
@@ -0,0 +1,13 @@
+package edu.school21.game;
+
+public class GameMap {
+	public static final int EMPTY = 0;
+	public static final int PLAYER = 1;
+	public static final int WALL = 2;
+	public static final int ENEMY = 3;
+	public static final int PORTAL = 4;
+
+
+	private int[][] gameMap;
+
+}
