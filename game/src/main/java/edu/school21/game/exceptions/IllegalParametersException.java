e28120679df7f60e06bbacc8bca98f1b3fe545aa
Andrei Beseda <64425949+mondrew@users.noreply.github.com>
2021-07-05 12:53:25 +0300

    Delete game directory

--- game/src/main/java/edu/school21/game/exceptions/IllegalParametersException.java
+++
@@ -1,7 +0,0 @@
-package edu.school21.game.exceptions;
-
-public class IllegalParametersException extends RuntimeException {
-	public IllegalParametersException(String message) {
-		super(message);
-	}
-}
